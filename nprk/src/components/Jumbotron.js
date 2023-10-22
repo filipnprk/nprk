7bcfef3c163cb072d217344c8c9c557f35c62d11
filipnprk <filip@nprk.xyz>
2023-10-22 23:59:44 +0100

    added jumbotron styling and added background image

--- nprk/src/components/Jumbotron.js
+++ nprk/src/components/Jumbotron.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Jumbotron = () => {
+
+    const jumbotronStyle = {
+        backgroundImage: `url('/images/nprkbackg.png')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    };
+
     return (
-        <div className='flex items-center justify-center h-screen'>
-            <div className='text-center'>
-                <h1 className='text-4xl font-bold'> Welcome to Jumbotron Component </h1>
+        <div style={jumbotronStyle} className='flex items-baseline justify-center h-screen'>
+            <div className='text-center pt-4 mt-5'>
+                <h1 className='text-4xl font-bold pb-5'> Welcome to Jumbotron Component </h1>
                 <p className='text-lg mt-4'> A nicely designed jumbotron is a good jumbotron </p>
             </div>
         </div>
