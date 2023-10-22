e9cd339fbd5d8a9542126cf826da35a387ba9581
filipnprk <filip@nprk.xyz>
2023-10-22 14:11:08 +0100

    added Jumbotron component and applied to main app

--- nprk/src/App.js
+++ nprk/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 
 function App() {
   return (
    <div className="App">
     <Navbar />
+    <Jumbotron />
    </div>
   );
 }
---
+++ nprk/src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Jumbotron = () => {
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <div className='text-center'>
+                <h1 className='text-4xl font-bold'> Welcome to Jumbotron Component </h1>
+                <p className='text-lg mt-4'> A nicely designed jumbotron is a good jumbotron </p>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
